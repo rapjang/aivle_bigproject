bad14aa69a643b189c1f81376d3e1b7f3a93041e
rapjang <55730544+rapjang@users.noreply.github.com>
2023-07-11 13:27:48 +0900

    Update final_video.py
    url aivlebigproject-video.streamlit.app 수정

--- final_video.py
+++ final_video.py
@@ -51,7 +51,7 @@ def emotion():
     face_cascade = cv2.CascadeClassifier("./model/haarcascade_frontalface_default.xml")
     
     # Open Webcam
-    cap = cv2.VideoCapture("54.180.98.209:8501/video")
+    cap = cv2.VideoCapture("aivlebigproject-video.streamlit.app:8501/video")
     
     # Set the video capture resolution
     #cap.set(cv2.CAP_PROP_FRAME_WIDTH, 600)
