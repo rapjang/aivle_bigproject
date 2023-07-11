908c433e04c29b9fa988f4042da319f255206706
rapjang <55730544+rapjang@users.noreply.github.com>
2023-07-11 13:21:20 +0900

    Update final_video.py
    ip add

--- final_video.py
+++ final_video.py
@@ -51,7 +51,7 @@ def emotion():
     face_cascade = cv2.CascadeClassifier("./model/haarcascade_frontalface_default.xml")
     
     # Open Webcam
-    cap = cv2.VideoCapture("rtsp://admin:admin@10.10.220.151/stream0")
+    cap = cv2.VideoCapture("54.180.98.209:8501/video")
     
     # Set the video capture resolution
     #cap.set(cv2.CAP_PROP_FRAME_WIDTH, 600)
